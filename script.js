29758c73a3d8cc2663cd387cb79bc8b6c9f9542e
LiborArion <libor@arionetworks.com>
2024-12-31 10:36:42 +0000

    Update script.js

--- script.js
+++ script.js
@@ -21,3 +21,30 @@ function calculateResult() {
         display.value = 'Error';
     }
 }
+function openTab(evt, tabName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+function convertUnit() {
+  var input = document.getElementById("unitInput").value;
+  var type = document.getElementById("unitType").value;
+  var result;
+  
+  if (type === "metersToFeet") {
+    result = input * 3.28084;
+  } else if (type === "feetToMeters") {
+    result = input / 3.28084;
+  }
+  
+  document.getElementById("conversionResult").innerText = "Result: " + result;
+}
