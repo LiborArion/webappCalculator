8e0be196c2a470b74cd5ccc4ceec5f9a1af9bd01
LiborArion <libor@arionetworks.com>
2024-12-31 10:15:52 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+// Function to clear the display
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
 
+// Function to append value to the display
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+// Function to calculate the result
+function calculateResult() {
+    const display = document.getElementById('display');
+    try {
+        display.value = eval(display.value);
+    } catch (e) {
+        display.value = 'Error';
+    }
+}
